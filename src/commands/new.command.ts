a3031e3e6cfc82c6d57e909cccc4277028ab3b67
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-19 19:27:57 -0300

    Add project existence verification on "new" command

--- src/commands/new.command.ts
+++ src/commands/new.command.ts
@@ -1,5 +1,7 @@
 import chalk from "chalk";
+import { existsSync } from "fs";
 import inquirer from "inquirer";
+import { join } from "path";
 import { ArgumentsCamelCase } from "yargs";
 import StuartProjectManager from "../core/StuartProject/StuartProjectManager";
 import getAbsolutePath from "../helpers/get-absolute-path";
@@ -18,6 +20,11 @@ export default async function newCommand(args: ArgumentsCamelCase<NewCommandArgs
     ? getAbsolutePath(args.directory)
     : getAbsolutePath(sanitizeDirName(projectName));
 
+  if (existsSync(join(projectDirectory, "stuart.conf"))) {
+    console.log(chalk.red(`A project already exists at "${projectDirectory}".`));
+    return;
+  }
+
   const success = await StuartProjectManager.create({
     projectName,
     projectDirectory,
@@ -26,7 +33,7 @@ export default async function newCommand(args: ArgumentsCamelCase<NewCommandArgs
   });
 
   if (!success) {
-    return console.log(chalk.red("Failed to create project."));
+    return console.log(chalk.red("\nCancelling project creation operation."));
   }
 
   console.log(chalk.green(`\nProject "${projectName}" created successfully!`));
