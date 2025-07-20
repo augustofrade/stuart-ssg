b76519dfb6645f99452adae4d5ce8fd7372ee651
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-20 00:10:07 -0300

    Add ThemeBuilder for theme reading and caching in-memory

---
+++ src/core/StuartProject/builders/ThemeBuilder.ts
@@ -0,0 +1,34 @@
+import fs from "fs/promises";
+import path from "path";
+import StuartProject from "..";
+import BobLogger from "../../BobLogger";
+
+export default class ThemeBuilder {
+  private readonly logger = BobLogger.Instance;
+  private static instance: ThemeBuilder;
+  public themeContent: string | null = null;
+
+  public async loadCurrentTheme(): Promise<this> {
+    const theme = StuartProject.Instance.configs?.project_definition?.theme as string | undefined;
+    this.logger.logVerbose(`Loading theme: ${theme}`);
+    if (!theme) {
+      throw new Error("Could not find theme in project configuration.");
+    }
+
+    this.logger.logVerbose(`Reading theme...`);
+
+    // TODO: load css file based on md file name
+    const themePath = path.join(StuartProject.Instance.projectDirectory, "themes", theme);
+    const themeFile = path.join(themePath, "index.html");
+    this.themeContent = await fs.readFile(themeFile, "utf-8");
+
+    return this;
+  }
+
+  public static get Instance(): ThemeBuilder {
+    if (!ThemeBuilder.instance) {
+      ThemeBuilder.instance = new ThemeBuilder();
+    }
+    return ThemeBuilder.instance;
+  }
+}
