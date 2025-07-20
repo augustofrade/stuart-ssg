e33e8e6ddefb7c5cddd5ac5235626bbab1abcd95
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-20 00:13:20 -0300

    Add StuartProjectManager loadProject() and buildPage() methods

--- src/core/StuartProject/StuartProjectManager.ts
+++ src/core/StuartProject/StuartProjectManager.ts
@@ -1,8 +1,29 @@
+import StuartProject from ".";
+import StuartPageBuilder from "./builders/PageBuilder";
+import ThemeBuilder from "./builders/ThemeBuilder";
 import StuartProjectCreate from "./handlers/project-create";
 import { CreateStuartProjectOptions } from "./types";
 
 export default class StuartProjectManager {
   public static async create(options: CreateStuartProjectOptions): Promise<boolean> {
     return new StuartProjectCreate(options).handle();
+  }
+
+  public static async loadProject(directory: string): Promise<boolean> {
+    const project = StuartProject.Instance;
+    project.projectDirectory = directory;
+    try {
+      await project.readConfigFile();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  public static async buildPage(fullPagePath: string): Promise<void> {
+    await ThemeBuilder.Instance.loadCurrentTheme();
+    const pageBuilder = await new StuartPageBuilder().loadPage(fullPagePath);
+    await pageBuilder.parseMarkdown();
+    pageBuilder.injectTheme().build();
   }
 }
