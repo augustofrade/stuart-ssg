fa4a45cf53a7b56695c533af0dd7c2feeaddb9d3
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-20 00:10:40 -0300

    Add StuartPage class for page model operations

---
+++ src/core/StuartProject/StuartPage.ts
@@ -0,0 +1,61 @@
+import ConfigFile, { Config } from "../../helpers/ConfigFile";
+
+export default class StuartPage {
+  public readonly configs: Config;
+  public content: string;
+
+  public constructor(pageContent: string) {
+    const lines = pageContent.split("\n");
+    const [config, startOfFile] = this.getPageDefinition(lines);
+    this.configs = config;
+    this.content = lines.slice(startOfFile + 1).join("\n");
+  }
+
+  private getPageDefinition(lines: string[]): [Config, number] {
+    const start = lines.indexOf("[PAGE_DEFINITION]");
+    const end = lines.indexOf("[PAGE]", start);
+    if (start === -1 || end === -1) {
+      return [{}, 0];
+    }
+
+    let rawConfig = lines
+      .slice(start, end)
+      .filter((line) => line.trim() !== "")
+      .join("\n");
+
+    if (rawConfig === "") {
+      return [{}, 0];
+    }
+
+    return [ConfigFile.parse(rawConfig), end];
+  }
+
+  public async parse(parseMethod: (string: string) => Promise<string>): Promise<this> {
+    if (!this.content) {
+      throw new Error("Page content is empty. Cannot parse.");
+    }
+
+    this.content = await parseMethod(this.content);
+    return this;
+  }
+
+  public injectTheme(theme: string): this {
+    this.content = theme.replace("%PAGE_CONTENT%", this.content);
+    return this;
+  }
+
+  public injectProps(props: Record<string, Config>): this {
+    let builtTheme = this.content;
+    for (const [key, value] of Object.entries(props)) {
+      const placeholder = `%${key.toUpperCase()}%`;
+      if (typeof value === "string") {
+        builtTheme = builtTheme.replace(new RegExp(placeholder, "g"), value);
+      } else if (typeof value === "boolean" || typeof value === "number") {
+        builtTheme = builtTheme.replace(new RegExp(placeholder, "g"), String(value));
+      }
+    }
+    this.content = builtTheme;
+
+    return this;
+  }
+}
