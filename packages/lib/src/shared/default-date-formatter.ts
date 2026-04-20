0e214d9edf94acd55b4ac713d73b883f4569bf97
augustofrade <aquaaticnaotrava@gmail.com>
2026-04-20 20:23:17 -0300

    Improved defaultDateFormatter helper function

--- packages/lib/src/shared/default-date-formatter.ts
+++ packages/lib/src/shared/default-date-formatter.ts
@@ -1,5 +1,6 @@
 import { PROJECT_LOCALE } from "../core/project-locale";
 
-export function defaultDateFormatter(date: Date, locale?: string) {
+export function defaultDateFormatter(date: Date | string, locale?: string) {
+  if (typeof date === "string") date = new Date(date);
   return date.toLocaleString(locale ?? PROJECT_LOCALE.value);
 }
