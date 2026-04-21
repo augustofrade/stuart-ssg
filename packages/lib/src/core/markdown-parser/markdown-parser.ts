831a9d46688c84b7e838b9179d42f8a9b8c83028
augustofrade <aquaaticnaotrava@gmail.com>
2026-04-20 21:03:13 -0300

    Add MarkdownParser class to encapsulate marked package

---
+++ packages/lib/src/core/markdown-parser/markdown-parser.ts
@@ -0,0 +1,16 @@
+import { marked } from "marked";
+
+/**
+ * Generic markdown parsing helper class that encapsulates logic.
+ */
+export class MarkdownParser {
+  private readonly parser: Function;
+
+  constructor() {
+    this.parser = marked;
+  }
+
+  public parse(md: string) {
+    return this.parser(md);
+  }
+}
